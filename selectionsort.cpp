5568fca2db389d657620f53d0cdf748682455601
Gabriel Felip Mano Lasig <GM887@live.mdx.ac.uk>
2024-01-19 10:09:54 +0000

    tried implementing selection sort

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
 
 using namespace std;
 
@@ -8,12 +9,25 @@ class Stopwatch {
         clock_t starttime;
 
     public:
-        Stopwatch(int n);
-        void start(){
+        void start(vector<int> n){
             clock_t start = clock();
             /*
             selection sort algorithm to be written here
             */
+
+            for (int j=0; j<n.size()-2; j++){
+                int x=j;
+                for (int i=j+1; i<n.size()-1; i++){
+                    if (n[i] < n[x]){
+                        x = i;
+                    }
+                }
+
+                if (x/=j){
+                    n[j] = n[x];
+                }
+            }
+
             clock_t end = clock();
 
             double duration_in_seconds = static_cast<double>(end - start)/CLOCKS_PER_SEC;
@@ -23,9 +37,15 @@ class Stopwatch {
             return static_cast<double>(starttime);
         };
 
-}
+};
 
 
 int main(){
+    vector<int> array = {4, 5, 3, 2, 1};
+
+    Stopwatch obj;
+    obj.start(array);
+    obj.stop();
+
     return 0;
 }
